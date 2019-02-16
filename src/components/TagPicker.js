9c437695a7534d3ab3a4239c23d6f4845c66c825
Stormlight-Dev <nathan.pintar@gmail.com>
2019-02-16 12:51:16 -0600

    made the whole add-tag button a link rather than the text

--- src/components/TagPicker.js
+++ src/components/TagPicker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Tag from '../components/Tag';
-import {NavLink, Link} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const inputStyle = {
   margin: 0,
@@ -11,8 +11,8 @@ const inputStyle = {
 }
 
 const isChecked = (currentTags, alteredTag) => {
-  for(let i = 0; i < currentTags.length; i++){
-    if (currentTags[i].description === alteredTag.description){
+  for (let i = 0; i < currentTags.length; i++) {
+    if (currentTags[i].description === alteredTag.description) {
       return currentTags[i].isChecked
     }
   }
@@ -20,32 +20,35 @@ const isChecked = (currentTags, alteredTag) => {
 
 const dropMenuStyle = {
   display: 'inline-block',
-  whiteSpace : 'nowrap'
+  whiteSpace: 'nowrap'
 }
 
 const TagPicker = (props) => (
-  <div className="dropdown" style={{display: 'inline-block'}}>
-  <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Categories
+  <div className="dropdown" style={{ display: 'inline-block' }}>
+    <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Categories
   </button>
-  <div className="dropdown-menu">
-      {props.existingTags.map((tag) =>{
-      return (
-        <div key={tag.description}>
-        <div style = {dropMenuStyle}>
-          <input type="checkbox" checked={isChecked(props.currentTags, tag)} value={JSON.stringify(tag)} onChange={props.addTag}/>
-          <Link to={`/edit/tag/${tag.id}`}>{tag.description} </Link>  
-          <Tag id={tag.id} />
+    <div className="dropdown-menu">
+      {props.existingTags.map((tag) => {
+        return (
+          <div key={tag.description}>
+            <div style={dropMenuStyle}>
+              <input type="checkbox" checked={isChecked(props.currentTags, tag)} value={JSON.stringify(tag)} onChange={props.addTag} />
+              <Link to={`/edit/tag/${tag.id}`}>{tag.description} </Link>
+              <Tag id={tag.id} />
+            </div>
           </div>
-        </div>
-      )})}
+        )
+      })}
       <div className="d-flex justify-content-center">
-      <button type="button" className="text-center">  
-        <NavLink to="/tags">Add New Tag</NavLink>
+        <NavLink to="/tags">
+          <button type="button" className="text-center">
+            Add New Tag
       </button>
+        </NavLink>
       </div>
     </div>
-</div>
+  </div>
 )
 
 const mapStateToProps = (state) => {
